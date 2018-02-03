1daf0c96ef6187e96512c5ec02b2e206cc9a17f4
Travis Brooks <tbrooks3141@gmail.com>
2018-02-03 13:46:07 -0800

    Add ScoreManager class to persistence layer with GetHighScore static method. Add high score and champ to footer in _Layout partial.

---
+++ Divisicus.Persistence/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Divisicus.Persistence
+{
+    public class ScoreManager
+    {
+
+        public static string GetHighScore()
+        {
+            var db = new LevelScoreEntities();
+            int highScore = db.Players.Max(p => p.HighScore);
+            Player champ = db.Players.FirstOrDefault(p => p.HighScore == highScore);
+            return highScore.ToString() + " - " + champ.Alias;
+        }
+    }
+}
